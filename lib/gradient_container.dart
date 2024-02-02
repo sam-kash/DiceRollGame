423196406ec969eac80464b5222070034dee5b13
sam-kash <kashyapsankalpa@gmail.com>
2024-02-03 00:30:02 +0530

    Introduction of Variables, its types and include 'const', 'final'

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,6 +1,9 @@
 import 'package:first_app/styled_text.dart';
 import 'package:flutter/material.dart';
-import 'package:first_app/styled_text.dart';
+
+const startAlignment = Alignment.topLeft;
+const endAlignment = Alignment
+    .bottomRight; // Here we are initializing the value and putting it into use
 
 class GradientContainer extends StatelessWidget {
   // GradientContainer({key}) : super(key: key);
@@ -14,13 +17,14 @@ class GradientContainer extends StatelessWidget {
   {
     return Container(
       decoration: const BoxDecoration(
+        // Here I am removing the const before BoxDecoration because the variables are not accepting a constant value as it can constantly changed
         gradient: LinearGradient(
           colors: [
             Color.fromARGB(255, 29, 11, 79),
             Color.fromARGB(218, 12, 3, 70),
           ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
+          begin: startAlignment,
+          end: endAlignment,
         ),
       ),
       child: const Center(
