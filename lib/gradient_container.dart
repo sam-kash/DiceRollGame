04f795b8bff043ed576f9f52dc2538589cf7ec32
sam-kash <kashyapsankalpa@gmail.com>
2024-02-10 01:16:27 +0530

    add finctionality to the button that I added

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -8,14 +8,17 @@ const endAlignment = Alignment
 // Another Method
 
 class GradientContainer extends StatelessWidget {
-  const GradientContainer(this.color1, this.color2,
+  GradientContainer(this.color1, this.color2,
       {super.key}); // Const unlocks your code to be optimizable
 
   final Color color1;
   final Color color2;
+  var activeDiceImage = 'assets/images/dice-2.png';
 
   void rolldice() {
-    // The body of this function
+    activeDiceImage = 'assets/images/dice-4.png';
+    print(
+        "Rolling it ..."); //We use it to see if the function is working or not, only the developer can actually see this
   }
 
   @override
@@ -33,14 +36,24 @@ class GradientContainer extends StatelessWidget {
         ),
       ),
       child: Center(
-          child: Column(children: [
+          child: Column(mainAxisSize: MainAxisSize.min, children: [
         Image.asset(
-          'assets/images/dice-2.png',
+          activeDiceImage,
           width: 200,
         ),
+        const SizedBox(
+            height:
+                20), //Its a widget that is creating a box of height 20 pixels between image and the button which is acting like a padding
+
         TextButton(
           onPressed:
               rolldice, // WE using this approach where we define a method or a function in the functino define area and then calling it by passing a pointer using function name
+          style: TextButton.styleFrom(
+              // padding: const EdgeInsets.only(
+              //   top: 20,
+              // ), //Showing a different approach, this one is perfectly fine tho
+              foregroundColor: Colors.white,
+              textStyle: const TextStyle(fontSize: 23)),
           child: const Text('Roll DICE'),
         )
       ]) // Image is a function , Can Define Multiple Constructor Function into Dart
--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,11 @@ import 'package:first_app/gradient_container.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
       home: Scaffold(
         //backgroundColor: Color.fromARGB(255, 88, 43, 210),
-        body: GradientContainer(
-            Color.fromARGB(255, 66, 10, 112), Color.fromARGB(255, 21, 55, 134)),
+        body: GradientContainer(const Color.fromARGB(255, 66, 10, 112),
+            const Color.fromARGB(255, 21, 55, 134)),
       ),
     ),
   ); //Blue Lines suggest that the code has potential to be optimized
